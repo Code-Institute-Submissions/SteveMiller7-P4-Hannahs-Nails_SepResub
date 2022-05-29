dd909d9b6290ea53157d069d6fb1f7817d1654c2
SteveMiller7 <info@millercreativedesign.com>
2022-05-29 15:33:12 +0000

    Added TechnicianList class to views.py

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Technician
 
 
-# Create your views here.
+class TechnicianList(generic.ListView):
+    model = Technician
+    template_name = 'index.html'
+    paginate_by = 6
+    # paginate_by wont be used as there will not be more than 6 technicians. This for my reference only
+    
