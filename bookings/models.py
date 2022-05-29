bc86b0998150ef8b5cf998933f7a028ba1b7f6fa
SteveMiller7 <info@millercreativedesign.com>
2022-05-29 14:18:21 +0000

    Create User class but commented out for now

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from address.models import AddressField
 
 
 class Technician(models.Model):
@@ -10,3 +11,14 @@ class Technician(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# class User(models.Model):
+#     title = models.CharField(max_length=40, unique=True)
+#     email = models.EmailField(unique=True)
+#     phone = models.IntegerField(max_length=11, unique=True)
+#     # address_1 = models.CharField(_("Address line 1"), max_length=100)
+#     # address_2 = models.CharField(_("Address line 2"), max_length=100)
+#     # city = models.CharField(_("City/Town"), max_length=30)
+#     # post_code = models.CharField(_("Postcode"), max_length=10)
+#     address = AddressField()
