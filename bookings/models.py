1cc276878ff56c3e5ab3d4d8684a859114ca6770
SteveMiller7 <info@millercreativedesign.com>
2022-05-29 14:07:27 +0000

    Created Technician model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Technician(models.Model):
+    title = models.CharField(max_length=40, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    background_info = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
