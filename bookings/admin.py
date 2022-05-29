d70d03d2ddc51d7d1163ab3b5c5d5f6afd8ed6f1
SteveMiller7 <info@millercreativedesign.com>
2022-05-29 14:54:38 +0000

    migrated Customer model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Technician, Customer
 
-# Register your models here.
+admin.site.register(Technician)
+admin.site.register(Customer)
--- bookings/models.py
+++ bookings/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from address.models import AddressField
 
 
 class Technician(models.Model):
@@ -13,12 +12,14 @@ class Technician(models.Model):
         return self.title
 
 
-# class User(models.Model):
-#     title = models.CharField(max_length=40, unique=True)
-#     email = models.EmailField(unique=True)
-#     phone = models.IntegerField(max_length=11, unique=True)
-#     # address_1 = models.CharField(_("Address line 1"), max_length=100)
-#     # address_2 = models.CharField(_("Address line 2"), max_length=100)
-#     # city = models.CharField(_("City/Town"), max_length=30)
-#     # post_code = models.CharField(_("Postcode"), max_length=10)
-#     address = AddressField()
+class Customer(models.Model):
+    title = models.CharField(max_length=40, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.IntegerField(unique=True)
+    # address_1 = models.CharField(_("Address line 1"), max_length=100)
+    # address_2 = models.CharField(_("Address line 2"), max_length=100)
+    # city = models.CharField(_("City/Town"), max_length=30)
+    # post_code = models.CharField(_("Postcode"), max_length=10)
+
+    def __str__(self):
+        return self.title
