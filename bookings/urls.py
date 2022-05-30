302732e6a9d25f2b074862b11a11e0d106491795
SteveMiller7 <info@millercreativedesign.com>
2022-05-30 13:41:25 +0000

    removed nav. Trying to set the site up correctly

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.TechnicianList.as_view(), name="bookings"),
+    path ('', views.TechnicianList.as_view(), name="home")
     # Blank path indicates its the default (home page)
+
 ]
