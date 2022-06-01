277d0fab734488f9dbde16b22253ebe86202a1d8
SteveMiller7 <info@millercreativedesign.com>
2022-06-01 14:24:53 +0000

    Hook up urls for appointment_bookings.html and 'Make a Booking' button on index page

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.Index, name='index'),
     path('inspiration/', views.Inspiration.as_view(), name='photos'),
-    path('appointment_bookings/', views.TechnicianList.as_view(), name="appointment_bookings"),
+    path('appointment_bookings/', views.NewBooking, name="appointment_bookings"),
     # Blank path indicates its the default (home page)
 
 ]
